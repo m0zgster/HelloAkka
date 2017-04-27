71159adc4322eb3571783180acc8c3e565d4efb5
Maxim Shalashov <maxim.shalashov@paragon-software.com>
2017-04-27 11:31:58 +0300

    hello akka

--- build.sbt
+++ build.sbt
@@ -3,4 +3,8 @@ name := "HelloAkka"
 version := "1.0"
 
 scalaVersion := "2.12.2"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.0"
+)
     
--- src/main/scala/HelloAkka.scala
+++ src/main/scala/HelloAkka.scala
@@ -1,4 +1,24 @@
+import akka.actor.{Actor, ActorSystem, Props}
 
-class HelloAkka {
+//a message
+case class WhoToGreet(who: String)
+
+//an actor
+class Greeter extends Actor {
+
+  override def receive = {
+    case WhoToGreet(who) => println(s"Hello $who")
+  }
+
+}
+
+object HelloAkkaScala extends App {
+
+  val system = ActorSystem("Hello-Akka")
+
+  val greeter = system.actorOf(Props[Greeter], "greeter")
+
+  //send a message to our actor
+  greeter ! WhoToGreet("Akka")
 
 }
